15a27805119975e333822fc87bded91bae78cb99
Robin Rydén <robinryden82@gmail.com>
2022-11-12 22:42:28 +0100

    chore: more slices

--- references_borrowing/src/main.rs
+++ references_borrowing/src/main.rs
@@ -16,7 +16,42 @@ fn main() {
     println!("{}", s2); // now prints Hello, world after change on memory
 
     // The Slice Type
-    
+    // A slice is a kind of reference, so it does not have ownership.
+    let mut s = String::from("hello world");
+
+    let _word = first_word(&s);
+
+    s.clear();
+
+    // println!("{}", word);
+
+    // Slices
+    let w = String::from("hello there world");
+
+    let hello = &w[0..5]; // also [..5] works 
+    let there = &w[6..11];
+    let world = &w[12..17]; // also [12..] works, index 12 to the end of index
+
+    println!("first slice: {}, second slice: {}, third slice: {}", hello, there, world);
+
+    // other slices
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[2..5];
+
+    assert_eq!(slice, &[3, 4, 5]);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
 
 fn calculate_length(s: &String) -> usize {
