0004fc990b112b59052f48f01fe39d186aeae267
Robin Rydén <robinryden82@gmail.com>
2022-11-12 23:05:03 +0100

    chore: ways of working with structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,13 +1,13 @@
-fn main() {
-    struct Person {
-        name: String,
-        age: u32,
-        address: String,
-        online: bool,
-    }
+struct Person {
+    name: String,
+    age: u32,
+    address: String,
+    online: bool,
+}
 
-    let p: Person = Person {
-        name: String::from("Robin"),
+fn main() {
+    let mut p: Person = Person {
+        name: String::from("Robin"), // can't put mut on specific fields
         age: 25,
         address: String::from("Codeway 99"),
         online: true,
@@ -19,4 +19,36 @@ fn main() {
         p.address,
         p.online
     );
+
+    p.name = String::from("Lobin");
+
+    println!("Person first name is now: {}", p.name);
+
+    // create person and print age
+    let p2 = create_person(
+        String::from("Lubin"),
+        33,
+        String::from("Codeway 124"),
+        false,
+    );
+
+    println!("Person 2's age is: {}", p2.age);
+
+    let p3 = Person {
+        age: 100,
+        ..p2        // <- .. specifies that the remaining fields not explicitly set 
+                    // should have the same value as the fields in the given instance
+    };
+
+    println!("Person 3 name = {}, age = {}", p3.name, p3.age);
+}
+
+// Create person and return person struct populated with data
+fn create_person(name: String, age: u32, address: String, online: bool) -> Person {
+    Person {
+        name,
+        age,
+        address,
+        online,
+    }
 }
