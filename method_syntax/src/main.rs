3d9580ecd815703c52737dfe5e28fbf1a23701c2
Robin Rydén <robinryden82@gmail.com>
2022-11-14 23:51:12 +0100

    chore: methods, impl, self

--- method_syntax/src/main.rs
+++ method_syntax/src/main.rs
@@ -1,3 +1,25 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+// methods are defined within the context of a struct (or an enum or a trait object
+// Everything within this impl block will be associated with the Rectangle type
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect = Rectangle {
+        width: 50,
+        height: 50,
+    };
+
+    println!(
+        "The are of the rectangle is {} square pixels.",
+        rect.area()
+    );
 }
