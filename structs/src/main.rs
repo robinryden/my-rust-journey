77866061d65d651b8644f66875b82fede9aae756
Robin Rydén <robinryden82@gmail.com>
2022-11-14 23:35:18 +0100

    chore: more structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -2,14 +2,34 @@ struct Person {
     name: String,
     age: u32,
     address: String,
+    car: Car,
     online: bool,
 }
+
+struct Car {
+    model: String,
+    year: String,
+    brand: String,
+}
+
+// Unit-Like Structs Without Any Fields
+// can be useful when you need to implement a trait on some type 
+// but don’t have any data that you want to store in the type itself.
+struct Placeholder;
+
+#[derive(Debug)]
+struct Coordinates(f32, f32);
 
 fn main() {
     let mut p: Person = Person {
         name: String::from("Robin"), // can't put mut on specific fields
         age: 25,
         address: String::from("Codeway 99"),
+        car: Car {
+            model: String::from("Tesla Plaid"),
+            year: String::from("2022"),
+            brand: String::from("Tesla"),
+        },
         online: true,
     };
 
@@ -18,6 +38,12 @@ fn main() {
         p.age,
         p.address,
         p.online
+    );
+
+    println!("Person car brand: {}, model: {}, year: {}", 
+        p.car.brand,
+        p.car.model,
+        p.car.year
     );
 
     p.name = String::from("Lobin");
@@ -41,6 +67,12 @@ fn main() {
     };
 
     println!("Person 3 name = {}, age = {}", p3.name, p3.age);
+
+    let coordinates = Coordinates(54.383727222, 23.42832632);
+
+    println!("{:?}", coordinates);
+
+    let placeholder = Placeholder;
 }
 
 // Create person and return person struct populated with data
@@ -49,6 +81,11 @@ fn create_person(name: String, age: u32, address: String, online: bool) -> Perso
         name,
         age,
         address,
+        car: Car {
+            model: String::from("Tesla Plaid"),
+            year: String::from("2022"),
+            brand: String::from("Tesla"),
+        },
         online,
     }
 }
