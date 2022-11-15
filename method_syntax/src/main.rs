760ca33bc37aa34dec78c2fab87695c36e20b4b5
Robin Rydén <robinryden82@gmail.com>
2022-11-15 19:18:22 +0100

    chore: methods, self

--- method_syntax/src/main.rs
+++ method_syntax/src/main.rs
@@ -10,6 +10,21 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -18,8 +33,25 @@ fn main() {
         height: 50,
     };
 
+    let other_rect = Rectangle {
+        width: 49,
+        height: 35,
+    };
+
     println!(
         "The are of the rectangle is {} square pixels.",
         rect.area()
     );
+
+    if rect.width() {
+        println!(
+            "The rectangle has a nonzero width; it is {}",
+            rect.width
+        );
+    }
+
+    println!("Can rect hold other_rect? {}", rect.can_hold(&other_rect));
+
+    let sq = Rectangle::square(10);
+    dbg!("sq = {}", sq);
 }
